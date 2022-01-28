d5820b5a6a64396260d206eda5f21a12950d27d8
Luca Geatti <luca.geatti@gmail.com>
2022-01-28 16:18:43 +0100

    change default layer

--- keyboards/ergodash/rev1/keymaps/lucageatti/keymap.c
+++ keyboards/ergodash/rev1/keymaps/lucageatti/keymap.c
@@ -19,23 +19,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * | ESC  |   1  |   2  |   3  |   4  |   5  |   `  |                    |      |   6  |   7  |   8  |   9  |   0  |  \   |
+   * | ESC  |   1  |   2  |   3  |   4  |   5  |   `  |                    | Bspc |   6  |   7  |   8  |   9  |   0  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |   -  |                    |   =  |   Y  |   U  |   I  |   O  |   P  | Ctrl |
+   * | Ctrl |   Q  |   W  |   E  |   R  |   T  |   -  |                    |   =  |   Y  |   U  |   I  |   O  |   P  | Ctrl |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Caps |   A  |   S  |   D  |   F  |   G  |  [   |                    |   ]  |   H  |   J  |   K  |   L  |   ;  |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Lower|   Z  |   X  |   C  |   V  |   B  | Space|                    | Enter|   N  |   M  |   ,  |   .  |   /  |      |
+   * | Lower|   Z  |   X  |   C  |   V  |   B  | Space|                    | Enter|   N  |   M  |   ,  |   .  |   /  | GUI  |
    * |-------------+------+------+------+------+      +------+------+------+      +------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | EISU |||||||| Shift| Space|                    | Enter| Shift|||||||| Left | Down |  Up  | Right|
+   * | Tab  |  GUI |  ALt | EISU |||||||| Shift| Space|                    | Enter| Shift|||||||| Left | Down |  Up  | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
-   */
+   */ 
   [_QWERTY] = LAYOUT(
-    KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,                         KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
-    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RCTL,
-    KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______,
-    LOWER,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-    KC_LCTL,  KC_LGUI, KC_LALT, EISU,             KC_LSFT, KC_SPC , _______,        _______,KC_ENT , KC_RSFT,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,                          KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+    KC_LCTL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                         KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RCTL,
+    KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                         KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______,
+    LOWER,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                         KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
+    KC_TAB,   KC_LGUI, KC_LALT, EISU,             KC_LSFT, KC_SPC , _______,        _______,KC_ENT , KC_RSFT,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   /* Lower
